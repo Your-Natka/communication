2bb27bf526a33af0df0592ee27496892562f564a
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2025-09-18 16:53:37 +0200

    start

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-white">Hello Tailwind!</h1>
+    </div>
+  )
+}
+
+export default App;
+
+
